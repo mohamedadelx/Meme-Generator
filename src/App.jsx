16315a6f78b4b4de27c856e93d3fe86a6a53b7f0
mohamed.salem2 <mohamed.salem@robustastudio.com>
2024-08-17 16:22:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Meme from "./Meme"
+import './style.css'
+function App() {
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="head">
+
+        <img className="header-image" src="./src/images/troll-face.png" alt="troll-face" />
+        <h1 className="header-title">Meme Generator</h1>
+        <h4 className="header-project">React Course - Project 3</h4>
+        
+    </header>
+  )
+}
+
+export default Header
